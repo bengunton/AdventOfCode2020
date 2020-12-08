0628ef419827be5c74c7e77b4be321be8407da09
Ben Gunton <bengunton@btinternet.com>
2020-12-08 22:30:17 +0000

    Day07 part 2

--- Day07/main.go
+++ Day07/main.go
@@ -6,6 +6,7 @@ import(
 	"os"
 	"bufio"
 	"strings"
+	"strconv"
 )
 
 func check(e error) {
@@ -14,10 +15,16 @@ func check(e error) {
 	}
 }
 
+type parent struct {
+	count int
+	name string
+}
+
 var RuleRegex = "^([a-z ]+) bags contain (?:((?:[0-9]+ [a-z ]+ bags?,? ?)+)|no other bags)\\."
-var ChildRegex = "[0-9]+ ((?:[a-z ])+) bags?"
+var ChildRegex = "([0-9]+) ((?:[a-z ])+) bags?"
 
 var containedIn = make(map[string][]string)
+var contains = make(map[string][]parent)
 
 func main() {
 	file, err := os.Open("input")
@@ -33,15 +40,14 @@ func main() {
 
 		matches := regex.FindStringSubmatch(row)
 		updateContainedIn(matches[1], strings.Split(matches[2], ","))
+		updateContains(matches[1], strings.Split(matches[2], ","))
 	}
-
-	// for key, values := range(containedIn) {
-	// 	fmt.Printf("%s: %v\n", key, values)
-	// }
 
 	foundParents := make(map[string]bool)
 
-	fmt.Printf("There are %d parents", countParents("shiny gold", foundParents))
+	fmt.Printf("There are %d parents\n", countParents("shiny gold", foundParents))
+
+	fmt.Printf("There are %d boxes in shiny gold", countContaining("shiny gold") - 1)
 }
 
 func updateContainedIn(subject string, children []string) {
@@ -53,13 +59,36 @@ func updateContainedIn(subject string, children []string) {
 			continue
 		}
 	
-		childName := matches[1]
+		childName := matches[2]
 
 		if containedIn[childName] == nil {
 			containedIn[childName] = make([]string, 0)
 		}
 
 		containedIn[childName] = append(containedIn[childName], subject)
+	}
+}
+
+func updateContains(subject string, children []string) {
+
+	regex := regexp.MustCompile(ChildRegex)
+	for _, child := range(children) {
+		matches := regex.FindStringSubmatch(child)
+		if matches == nil {
+			contains[subject] = make([]parent, 0)
+			continue
+		}
+	
+		count, err := strconv.Atoi(matches[1])
+		check(err)
+
+		childName := matches[2]
+
+		if contains[subject] == nil {
+			contains[subject] = make([]parent, 0)
+		}
+
+		contains[subject] = append(contains[subject], parent{count, childName})
 	}
 }
 
@@ -78,6 +107,16 @@ func countParents(target string, foundParents map[string]bool) int {
 
 		foundParents[parent] = true
 		sum += 1 + countParents(parent, foundParents)
+	}
+	return sum
+}
+
+func countContaining(target string) int {
+	children := contains[target]
+
+	sum := 1
+	for _, child := range(children) {
+		sum += child.count * countContaining(child.name)
 	}
 	return sum
 }
