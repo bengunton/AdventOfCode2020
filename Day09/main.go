02260a44f300068d18b2a6aff2d20d45f6c113ce
Ben Gunton <bengunton@btinternet.com>
2020-12-12 21:06:56 +0000

    Day 9 Part 02

--- Day09/main.go
+++ Day09/main.go
@@ -22,23 +22,32 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	buffer := make([]int, BUFFER_SIZE, BUFFER_SIZE)
+	fullList := make([]int, 0)
+	var target int
 
 	for i := 0; scanner.Scan(); i++ {
 		num, err := strconv.Atoi(scanner.Text())
+		fullList = append(fullList, num)
 		check(err)
 		
 		if (i >= BUFFER_SIZE) {
-			reportIfInvalid(num, buffer)
+			if !isValidEntry(num, buffer) {
+				target = num
+				fmt.Printf("Invalid entry: %d\n", num)
+			}
 		}
 
 		buffer[i % BUFFER_SIZE] = num
 
 	}
-}
 
-func reportIfInvalid(num int, buffer []int) {
-	if !isValidEntry(num, buffer) {
-		fmt.Printf("Invalid entry: %d\n", num)
+	for i, _ := range(fullList) {
+		foundSum, ints := checkForContinuousSumToN(fullList[i:], target)
+		if foundSum {
+			fmt.Println("Contiguous ints are", ints)
+			printMinAndMax(ints)
+			break;
+		}
 	}
 }
 
@@ -52,4 +61,34 @@ func isValidEntry(target int, buffer []int) bool {
 	}
 
 	return false
+}
+
+func checkForContinuousSumToN(ints []int, target int) (bool, []int) {
+	numsToSum := make([]int, 0)
+	for _, num := range(ints) {
+		target -= num
+		numsToSum = append(numsToSum, num)
+
+		if (target  < 0) {
+			break;
+		}
+		if (target == 0 && len(numsToSum) >= 2) {
+			return true, numsToSum
+		}
+	}
+
+	return false, nil
+}
+
+func printMinAndMax(ints []int) {
+	min, max := ints[0], ints[0]
+	for _, n := range(ints) {
+		if n > max {
+			max = n
+		}
+		if n < min {
+			min = n
+		}
+	}
+	fmt.Printf("Min is %d, max is %d, sum is %d\n", min, max, min + max)
 }
