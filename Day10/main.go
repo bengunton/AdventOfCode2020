a3b902542c4c1e4d68b733888b53d2d50691c295
Ben Gunton <bengunton@btinternet.com>
2020-12-13 00:29:08 +0000

    Day10 part2

--- Day10/main.go
+++ Day10/main.go
@@ -13,6 +13,7 @@ func check(err error) {
 		panic(err)
 	}
 }
+var memo map[int]int = make(map[int]int)
 
 func main() {
 	file, err := os.Open("input")
@@ -21,6 +22,7 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	// start with the wall (0)
 	joltages := make([]int, 1)
 	for scanner.Scan() {
 		num, err := strconv.Atoi(scanner.Text())
@@ -29,12 +31,15 @@ func main() {
 	}
 
 	sort.Ints(joltages)
+	// append the device (max + 3)
 	joltages = append(joltages, joltages[len(joltages) - 1] + 3)
 
 	oneJoltJumps, threeJoltJumps := countJoltJumps(joltages)
 
 	fmt.Printf("%d 1-jolt jumps and %d 3-jolt jumps\n", oneJoltJumps, threeJoltJumps)
-	fmt.Printf("Product is %d ", oneJoltJumps * threeJoltJumps)
+	fmt.Printf("Product is %d\n", oneJoltJumps * threeJoltJumps)
+
+	fmt.Printf("There are %d joltage combinations to charge the device\n", countCombinations(0, joltages[:(len(joltages) - 1)]))
 }
 
 func countJoltJumps(joltages []int) (int, int) {
@@ -54,3 +59,36 @@ func countJoltJumps(joltages []int) (int, int) {
 
 	return oneJoltJumps, threeJoltJumps
 }
+
+
+func countCombinations(startJolt int, joltages []int) int {
+	// save recalculating when we already know how many branches there are from this point
+	if (joltages[0] - startJolt) > 3 {
+		return 0
+	}
+	if (memo[joltages[0]] != 0) {
+		return memo[joltages[0]]
+	}
+	if (len(joltages) == 1) {
+		return 1
+	}
+
+	sum := 0
+	switch {
+	case len(joltages) > 3:
+		sum += countCombinations(joltages[0], joltages[3:])
+		fallthrough
+	case len(joltages) > 2:
+		sum += countCombinations(joltages[0], joltages[2:])
+		fallthrough
+	case len(joltages) > 1:
+		sum += countCombinations(joltages[0], joltages[1:])
+	}
+
+	// save value once we know it the first time
+	if memo[joltages[0]] == 0 {
+		memo[joltages[0]] = sum
+	}
+	return sum
+}
+
